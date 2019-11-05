5d71ea83993f6c2c271f56ed171b80006049f20d
shrki416 <coach-ahmed@bmsnj.org>
2019-11-05 13:58:38 -0500

    add an eventListener on the checkbox input

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function TodoList(props) {
     return (
         <div className='todo-item'>
-            <input type='checkbox' checked={props.item.completed}/>
+            <input onClick={() => console.log('checked')} type='checkbox' checked={props.item.completed}/>
             <p>{props.item.text}</p>
         </div>
     )
