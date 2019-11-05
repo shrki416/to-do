4429400e74ca34bd5f341f5c275d8af1389b6e86
shrki416 <coach-ahmed@bmsnj.org>
2019-11-05 12:46:49 -0500

    add a data for our todos

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function TodoList() {
+export default function TodoList(props) {
     return (
         <div className='todo-item'>
-            <input type='checkbox' />
-            <p>Placeholder text here</p>
+            <input type='checkbox' checked={props.item.completed}/>
+            <p>{props.item.text}</p>
         </div>
     )
 }
