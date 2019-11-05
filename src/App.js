c1b82fa59cd43aa01c0b082953be80a6d9bcda7d
shrki416 <coach-ahmed@bmsnj.org>
2019-11-05 13:48:08 -0500

    change from function based to class based component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import TodoItem from './TodoItem'
 import todosData from './todosData'
 
-function App() {
-  const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
-
-  return (
-    <div className='todo-list'>
-      {todoItems}
-    </div>
-  )
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todosData
+    }
+  }
+  render() {
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
+    return (
+      <div className='todo-list'>
+        {todoItems}
+      </div>
+    )
+  }
 }
 
 export default App
