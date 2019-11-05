599bf954ee0dbf4610d7a143e9021d02492f0e31
shrki416 <coach-ahmed@bmsnj.org>
2019-11-05 14:07:26 -0500

    delete uncessary files

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function TodoList(props) {
     return (
         <div className='todo-item'>
-            <input onClick={() => console.log('checked')} type='checkbox' checked={props.item.completed}/>
+            <input 
+                type='checkbox' 
+                checked={props.item.completed}
+                onChange={() => console.log('changed!')}
+                />
             <p>{props.item.text}</p>
         </div>
     )
