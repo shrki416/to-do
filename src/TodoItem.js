dbb68b670c8c3742fd397ad8dc9a57f88d5af332
shrki416 <coach-ahmed@bmsnj.org>
2019-11-06 13:35:17 -0500

    checked todo functionality complete

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -6,7 +6,7 @@ export default function TodoList(props) {
             <input 
                 type='checkbox' 
                 checked={props.item.completed}
-                onChange={() => console.log('changed!')}
+                onChange={() => props.handleChange(props.item.id)}
                 />
             <p>{props.item.text}</p>
         </div>
